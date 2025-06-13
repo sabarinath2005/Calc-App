797bfadaf01ec5d080bef2b299931df19b17c966
sabarinath2005 <165458612+sabarinath2005@users.noreply.github.com>
2025-06-13 21:54:02 +0530

    Calc-App

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -32,7 +32,7 @@ const Voting = () => {
 
   return (
     <div>
-        <Navbar/> <br />
+        <Navbar/>
       <div className="container">
         <div className="row">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
